a2f4fe2a83ed017a638dc2a3ab8530e84b46bbcb
mdovi474bd-lgtm <mdovi474bd@gmail.com>
2026-01-06 21:17:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-1
+const threePieceImages = [
+  "products/three-piece/p1.jpg",
+  "products/three-piece/p2.jpg",
+  "products/three-piece/p3.jpg"
+];
+
+const sareeImages = [
+  "products/saree/s1.jpg",
+  "products/saree/s2.jpg"
+];
+
+function loadProducts(id, images) {
+  const box = document.getElementById(id);
+  images.forEach(src => {
+    const img = document.createElement("img");
+    img.src = src;
+    box.appendChild(img);
+  });
+}
+
+loadProducts("threePiece", threePieceImages);
+loadProducts("saree", sareeImages);
