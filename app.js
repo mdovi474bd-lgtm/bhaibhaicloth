9949a399b25cc5f5fe602dc7b192e60d9ccdc2e5
mdovi474bd-lgtm <mdovi474bd@gmail.com>
2026-01-06 20:58:36 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+const categories = [
+  "saree",
+  "three-piece",
+  "hosiery",
+  "fabric"
+];
 
+const productBox = document.getElementById("products");
+
+categories.forEach(cat => {
+  fetch(`products/${cat}/`)
+    .then(() => {
+      for (let i = 1; i <= 10; i++) {
+        const img = document.createElement("img");
+        img.src = `products/${cat}/${cat[0]}${i}.jpg`;
+        img.onerror = () => img.remove();
+
+        const div = document.createElement("div");
+        div.className = "product";
+        div.appendChild(img);
+
+        productBox.appendChild(div);
+      }
+    });
+});
