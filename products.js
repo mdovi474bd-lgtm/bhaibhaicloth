80eb68506652ca3b1a918c8ca65cf1ce48a32c84
mdovi474bd-lgtm <mdovi474bd@gmail.com>
2026-01-06 13:41:43 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,31 @@
+const products = {
+  "শাড়ী": {
+    folder: "images/saree/",
+    images: ["s1.jpg", "s2.jpg", "s3.jpg"]
+  },
+
+  "থ্রী পিস": {
+    folder: "images/three-piece/",
+    images: ["t1.jpg", "t2.jpg"]
+  },
+
+  "লুঙ্গী": {
+    folder: "images/lungi/",
+    images: ["l1.jpg"]
+  },
+
+  "হোসিয়ারী": {
+    folder: "images/hosiery/",
+    images: ["h1.jpg"]
+  },
+
+  "তৈয়লা": {
+    folder: "images/tayela/",
+    images: ["ty1.jpg"]
+  },
+
+  "থান কাপড়": {
+    folder: "images/than/",
+    images: ["th1.jpg"]
+  }
+};
